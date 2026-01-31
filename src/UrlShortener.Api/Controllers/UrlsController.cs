b54826985fe9706258e87827a1062a0ac7dfe6d7
DESKTOP-B07HR9G\skiskidaiski <infedinwork@gmail.com>
2026-01-31 17:46:30 +0300

    feat: implement URL shortening api endpoint

---
+++ src/UrlShortener.Api/Controllers/UrlsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlShortener.Application.Contracts;
+using UrlShortener.Application.Services.Interfaces;
+
+namespace UrlShortener.Api.Controllers
+{
+    [Route("api/urls")]
+    [ApiController]
+    public class UrlsController : ControllerBase
+    {
+        private readonly IUrlShortenerService _urlShortenerService;
+        public UrlsController(IUrlShortenerService urlShortenerService)
+        {
+            _urlShortenerService = urlShortenerService;
+        }
+
+        [HttpPost("shorten")]
+        public async Task<IActionResult> ShortenUrl([FromBody] CreateShortUrlRequest request)
+        {
+            var response = await _urlShortenerService.CreateShortUrl(request);
+            return Ok(response);
+        }
+    }
+}
