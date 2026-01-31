e95904319a00e29b0ec5cf3f65badf83f282aca0
DESKTOP-B07HR9G\skiskidaiski <infedinwork@gmail.com>
2026-01-31 17:11:24 +0300

    Add initial migrations

---
+++ src/UrlShortener.Infrastructure/Migrations/20260130141601_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlShortener.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "short_url",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    original_url = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: false),
+                    short_code = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("id", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_short_url_original_url",
+                table: "short_url",
+                column: "original_url",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_short_url_short_code",
+                table: "short_url",
+                column: "short_code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "short_url");
+        }
+    }
+}
