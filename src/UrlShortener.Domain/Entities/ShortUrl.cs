9da3a7fbb38a3e9bfa094c0aaf7484b72f31c15d
DESKTOP-B07HR9G\skiskidaiski <infedinwork@gmail.com>
2026-01-31 17:08:44 +0300

    Add domain aggregate ShortUrl

---
+++ src/UrlShortener.Domain/Entities/ShortUrl.cs
@@ -0,0 +1,26 @@
+﻿namespace UrlShortener.Domain.Entities
+{
+    public sealed class ShortUrl
+    {
+        public Guid Id { get; init; }
+        public string OriginalUrl { get; init; } = null!;
+        public string ShortCode { get; init; } = null!;
+        private ShortUrl(Guid id, string originalUrl, string shortCode)
+        {
+            Id = id;
+            OriginalUrl = originalUrl;
+            ShortCode = shortCode;
+        }
+        private ShortUrl() { }
+
+        public static ShortUrl Create(string originalUrl, string shortCode)
+        {
+            if (string.IsNullOrWhiteSpace(originalUrl))
+                throw new ArgumentException("Original URL cannot be null or empty.", nameof(originalUrl));
+            if (string.IsNullOrWhiteSpace(shortCode))
+                throw new ArgumentException("Short code cannot be null or empty.", nameof(shortCode));
+
+            return new ShortUrl(Guid.NewGuid(), originalUrl, shortCode);
+        }
+    }
+}
---
+++ src/UrlShortener.Domain/Interfaces/IShortUrlRepository.cs
@@ -0,0 +1,12 @@
+﻿using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Domain.Interfaces
+{
+    public interface IShortUrlRepository
+    {
+        Task AddAsync(ShortUrl shortUrl);
+        Task<ShortUrl?> GetByOriginalUrlAsync(string originalUrl);
+        Task<ShortUrl?> GetByShortCodeAsync(string shortCode);
+        Task<bool> ExistsByShortCodeAsync(string  shortCode);
+    }
+}
