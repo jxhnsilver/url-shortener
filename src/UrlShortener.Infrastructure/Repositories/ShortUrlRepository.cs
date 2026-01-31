97a2e6489225e8f9518d51f3552d0896b6b89420
DESKTOP-B07HR9G\skiskidaiski <infedinwork@gmail.com>
2026-01-31 17:12:06 +0300

    Add ShortUrl repository implementation

---
+++ src/UrlShortener.Infrastructure/Repositories/ShortUrlRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Domain.Entities;
+using UrlShortener.Domain.Interfaces;
+using UrlShortener.Infrastructure.Context;
+
+namespace UrlShortener.Infrastructure.Repositories
+{
+    public class ShortUrlRepository : IShortUrlRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ShortUrlRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(ShortUrl shortUrl)
+        {
+            await _context.AddAsync(shortUrl);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> ExistsByShortCodeAsync(string shortCode)
+        {
+            return await _context.ShortUrls.AnyAsync(s => s.ShortCode == shortCode);
+        }
+
+        public async Task<ShortUrl?> GetByOriginalUrlAsync(string originalUrl)
+        {
+            return await _context.ShortUrls.FirstOrDefaultAsync(s => s.OriginalUrl == originalUrl);
+        }
+
+        public async Task<ShortUrl?> GetByShortCodeAsync(string shortCode)
+        {
+            return await _context.ShortUrls.FirstOrDefaultAsync(s => s.ShortCode == shortCode);
+        }
+    }
+}
