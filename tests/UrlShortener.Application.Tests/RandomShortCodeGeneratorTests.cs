75fa6b359e722a3b5fe8f8fcd904058c5c2f3d01
DESKTOP-B07HR9G\skiskidaiski <infedinwork@gmail.com>
2026-02-02 13:23:16 +0300

    Add tests for application layer

---
+++ tests/UrlShortener.Application.Tests/RandomShortCodeGeneratorTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+using UrlShortener.Application.Services.Implementations;
+
+namespace UrlShortener.Application.Tests
+{
+    public class RandomShortCodeGeneratorTests
+    {
+        [Theory]
+        [InlineData(6)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void Generate_WithValidLength_ReturnsStringWithCorrectLength(int length)
+        {
+            var generator = new RandomShortCodeGenerator(length);
+            var result = generator.Generate();
+
+            Assert.Equal(length, result.Length);
+        }
+
+        [Theory]
+        [InlineData(100, 10)]
+        public void Generate_ReturnsStringContainingAllowedChars(int iterations, int codeLength)
+        {
+            var generator = new RandomShortCodeGenerator(codeLength);
+            var pattern = $"^[{Regex.Escape(RandomShortCodeGenerator.Alphabet)}]+$";
+
+            for (int i = 0; i < iterations; i++)
+            {
+                var result = generator.Generate();
+                Assert.Matches(pattern, result);
+            }
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(11)]
+        public void Constructor_WhenCodeLengthOutOfRange_ThrowsArgumentOutOfRangeException(int invalidLength)
+        {
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() 
+                => new RandomShortCodeGenerator(invalidLength));
+            var message = "Short code length must be between 6 and 10";
+
+
+            Assert.Contains(message, exception.Message);
+            Assert.Equal("codeLength", exception.ParamName);
+        }
+    }
+}
